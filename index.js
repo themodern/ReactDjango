bba09934acb0acb8bab8cde63b0ae4f4a6d8ff6c
Ching Chan <65863602+themodern@users.noreply.github.com>
2023-10-10 16:30:14 +1100

    Add Mongo DB connection

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express"
+import dotenv from "dotenv"
+
 const app = express()
+dotenv.config()
 
 app.listen(8800, () => {
-    console.log("Connected to Backend :)")
+    console.log("Connected to Backend ")
 })
